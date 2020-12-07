d338b3c3b4c0bcbb091d1f2aa5859beba71dcf4e
slenderm97 <mikigreg97@gmail.com>
2020-12-07 20:47:12 +0100

    Show cards and score table

---
+++ dealer.py
@@ -0,0 +1,12 @@
+class Dealer:
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    @property.getter
+    def dealer_score(self):
+        pass
+
+    @property.setter
+    def dealer_score(self):
+        pass
--- main.py
+++ main.py
@@ -2,7 +2,13 @@ from tkinter import *
 from tkinter import messagebox
 from PIL import ImageTk, Image
 
+# Start frame
+root = Tk()
+root.title("Blackjack")
+root.geometry("960x640")
 
+
+# controllers
 def b_deal_on_click():
     messagebox.showinfo('Message', 'You clicked the Submit button!')
 
@@ -15,6 +21,7 @@ def b_stand_on_click():
     messagebox.showinfo('Message', 'You clicked the Submit button!')
 
 
+# UI
 def show_player_cards():
     #     get card
     #     show in player's area
@@ -27,10 +34,6 @@ def show_dealer_cards():
     pass
 
 
-root = Tk()
-root.title("Blackjack")
-root.geometry("960x640")
-
 bg_image = ImageTk.PhotoImage(Image.open("assets/background.jpg"))
 bg_label = Label(image=bg_image)
 bg_label.place(x=0, y=0)
@@ -40,5 +43,15 @@ b_hit = Button(text="HIT", command=b_hit_on_click, width=12, height=2)
 b_hit.place(x=420, y=560)
 b_stand = Button(text="STAND", command=b_stand_on_click, width=12, height=2)
 b_stand.place(x=540, y=560)
+
+image = Image.open("assets/spades/7.png")
+image = image.resize((102, 150), Image.ANTIALIAS)
+card_image = ImageTk.PhotoImage(image)
+image_label = Label(image=card_image)
+image_label.place(x=500, y=30)
+
+player_score = Text(root, height=2, width=20, bg='black', fg='yellow')
+player_score.place(x=20, y=20)
+player_score.insert(END, "asdasdasd")
 
 root.mainloop()
